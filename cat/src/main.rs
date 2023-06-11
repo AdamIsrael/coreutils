5abf444742c2573068137559811a802e01f30678
Adam Israel <adam@adamisrael.com>
2023-06-11 15:18:25 -0400

    Refactor (again)
    Refactored again, using the redox-os/coreutils/cat.rs as inspiration, and the logic in coreutils/cat.c to check for line count (-n)

--- cat/src/main.rs
+++ cat/src/main.rs
@@ -50,15 +50,15 @@ struct Args {
     files: Vec<String>,
 }
 
-// impl Args {
-//     fn tab(&self) -> &'static str {
-//         if self.show_tabs {
-//             "^I"
-//         } else {
-//             "\t"
-//         }
-//     }
-// }
+impl Args {
+    fn tab(&self) -> &'static str {
+        if self.show_tabs {
+            "I"
+        } else {
+            "\t"
+        }
+    }
+}
 
 fn main() {
     let mut args = Args::parse();
@@ -82,185 +82,128 @@ fn main() {
         args.show_tabs = true;
     }
 
-    let mut blank: i32 = 0;
+    // let mut blank: i32 = 0;
+    let mut line_count: usize = 1;
+    let mut stdout = io::stdout().lock();
+    let mut stderr = io::stderr().lock();
 
     if args.files.is_empty() {
-        let mut ln: i32 = 0;
-
         // read from stdin
-        let stdin = io::stdin();
+        let mut stdin = io::stdin().lock();
 
-        for line in stdin.lock().lines() {
-            let buf = line.unwrap();
-
-            if buf.trim().is_empty() {
-                blank += 1;
-            } else {
-                blank = 0;
-            }
-
-            if (args.squeeze_blank && blank >= 1) || (!args.number_nonblank && args.number) {
-                ln += 1;
-            }
-
-            // print to stdout
-            // if !args.squeeze_blank || args.squeeze_blank && blank < 1 {
-            if !(args.squeeze_blank && blank >= 1) {
-                output(&args, &buf, ln);
-            }
-        }
+        cat(&mut stdin, &args, &mut line_count, &mut stdout, &mut stderr);
     } else {
         for filename in &args.files {
             // TODO: match this
             let mut file = std::fs::File::open(filename).unwrap();
 
-            let mut data = Vec::new();
-            file.read_to_end(&mut data).expect("Unable to read data");
+            cat(&mut file, &args, &mut line_count, &mut stdout, &mut stderr);
+        }
+    }
 
-            // let mut out: String = String::new();
-            let stdout = io::stdout();
-            let mut writer = stdout.lock();
+    stdout.flush().unwrap();
+    stderr.flush().unwrap();
+}
 
-            // let mut out = Vec::new();
+/// Cat a file from argument or stdin
+fn cat<F: Read>(
+    file: &mut F,
+    args: &Args,
+    line_count: &mut usize,
+    stdout: &mut std::io::StdoutLock,
+    stderr: &mut std::io::StderrLock,
+) {
+    let mut character_count = 0;
+    let mut last_line_was_blank = false;
+    let mut in_buffer: [u8; 8 * 8192] = [0; 8 * 8192];
+    let mut out_buffer: Vec<u8> = Vec::with_capacity(24 * 8192);
+    loop {
+        let n_read = file.read(&mut in_buffer).unwrap();
+        if n_read == 0 {
+            break;
+        }
 
-            // let mut pos = 0;
-            for ch in data {
-                // match ch {
-                //     0..=8 | 10..=31 => writer.write_all(&[b'^', ch + 64]),
-                //     // 7 => writer.write_all(&[b'^', ch + 64]),
-                //     9 => writer.write_all(args.tab().as_bytes()),
-                //     33..=126 => writer.write_all(&[b'M', b'-']),
-                //     127 => writer.write_all(&[b'^', b'?']),
-                //     128..=159 => writer.write_all(&[b'M', b'-', ch - 64]),
-                //     160..=254 => writer.write_all(&[b'M', b'-', ch - 128]),
-                //     _ => writer.write_all(&[b'M', b'-', b'^', b'?']),
-                // }
-                // .unwrap();
-
-                match ch {
-                    0..=8 => {
-                        if args.show_nonprinting {
-                            writer.write_all(&[ch + 64]).unwrap();
-                        }
-                    }
-                    9 => {
-                        if args.show_tabs {
-                            writer.write_all(&[b'I']).unwrap();
-                        } else {
-                            writer.write_all(&[ch]).unwrap();
-                        }
-                    }
-                    10 => {
-                        if args.show_ends {
-                            writer.write_all(b"$\n").unwrap();
-                        } else {
-                            writer.write_all(b"\n").unwrap();
-                        }
-                    }
-                    32..=126 => {
-                        writer.write_all(&[ch]).unwrap();
-                    }
-                    127 => {}
-                    128..=159 => {
-                        if args.show_nonprinting {
-                            writer.write_all(b"M-^").unwrap();
-                            writer.write_all(&[ch - 64]).unwrap();
-                        } else {
-                            writer.write_all(&[ch]).unwrap();
-                        }
-                    }
-                    _ => {
-                        if args.show_nonprinting {
-                            writer.write_all(b"M-").unwrap();
-                            writer.write_all(&[ch - 128]).unwrap();
-                        } else {
-                            writer.write_all(&[ch]).unwrap();
-                        }
-                    }
-                };
+        for &byte in in_buffer[0..n_read].iter() {
+            if character_count == 0
+                && (args.number || (args.number_nonblank && byte != b'\n'))
+                && !last_line_was_blank
+            {
+                out_buffer
+                    .write_all(format!("{: >6}  ", line_count).as_bytes())
+                    .unwrap();
+                last_line_was_blank = true;
             }
 
-            writer.flush().unwrap();
+            match byte {
+                0..=8 | 11..=31 => {
+                    if args.show_nonprinting {
+                        push_caret(&mut out_buffer, stderr, byte + 64);
+                        count_character(&mut character_count, args);
+                    }
+                }
+                9 => {
+                    out_buffer.write_all(args.tab().as_bytes()).unwrap();
+                    count_character(&mut character_count, args);
+                }
+                10 => {
+                    if character_count == 0 {
+                        if args.squeeze_blank && last_line_was_blank {
+                            continue;
+                        } else if !last_line_was_blank {
+                            last_line_was_blank = true;
+                        }
+                    } else {
+                        last_line_was_blank = false;
+                        character_count = 0;
+                    }
+                    *line_count += 1;
 
-            // let s = match str::from_utf8(&out) {
-            //     Ok(v) => v,
-            //     Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-            // };
-
-            // println!("{}", s);
-
-            // // pretty sure I'm going to have to rewrite this whole section to read
-            // // in possible binary files. As-is, this works for text but will fail
-            // // hard if there's non-utf8 data, i.e., `cat -v /bin/bash`
-            // let mut reader = io::BufReader::new(file);
-            // let mut buf = String::new();
-            // let mut ln: i32 = 0;
-
-            // while reader.read_line(&mut buf).unwrap() > 0 {
-            //     if buf.trim().is_empty() {
-            //         blank += 1;
-            //     } else {
-            //         blank = 0;
-            //     }
-
-            //     // This isn't the prettiest, but it's functional
-            //     if (args.squeeze_blank && blank >= 1)
-            //         || (!args.number_nonblank && args.number)
-            //         || (args.number_nonblank && blank == 0)
-            //     {
-            //         ln += 1;
-            //     }
-
-            //     // print to stdout
-            //     if !(args.squeeze_blank && blank >= 1) {
-            //         output(&args, &buf, ln);
-            //     }
-
-            //     // clear the buffer for the next read
-            //     buf.clear();
-            // }
+                    if args.show_ends {
+                        out_buffer.write_all(b"$\n").unwrap();
+                    } else {
+                        out_buffer.write_all(b"\n").unwrap();
+                    }
+                }
+                32..=126 => {
+                    out_buffer.write_all(&[byte]).unwrap();
+                    count_character(&mut character_count, args);
+                }
+                127 => {
+                    push_caret(&mut out_buffer, stderr, b'?');
+                    count_character(&mut character_count, args);
+                }
+                128..=159 => {
+                    if args.show_nonprinting {
+                        out_buffer.write_all(b"M-^").unwrap();
+                        out_buffer.write_all(&[byte - 64]).unwrap();
+                    } else {
+                        out_buffer.write_all(&[byte]).unwrap();
+                    }
+                    count_character(&mut character_count, args);
+                }
+                _ => {
+                    if args.show_nonprinting {
+                        out_buffer.write_all(b"M-").unwrap();
+                        out_buffer.write_all(&[byte - 128]).unwrap();
+                    } else {
+                        out_buffer.write_all(&[byte]).unwrap();
+                    }
+                    count_character(&mut character_count, args);
+                }
+            };
         }
+        stdout.write_all(&out_buffer).unwrap();
+        out_buffer.clear();
     }
 }
 
-/// Output the string according to stdargs
-// going to have to change this to Vec<str> I think, so we can number lines
-fn output(args: &Args, line: &str, number: i32) {
-    let mut s = line.to_owned();
-
-    // strip the line ending; we'll add our own
-    if s.ends_with('\n') {
-        s.pop();
-        if s.ends_with('\r') {
-            s.pop();
-        }
+fn count_character(character_count: &mut usize, args: &Args) {
+    if args.number || args.number_nonblank {
+        *character_count += 1;
     }
+}
 
-    // display $ at the end of each line
-    if args.show_ends {
-        s += "$";
-    }
-
-    // show tabs
-    if args.show_tabs {
-        s = s.replace('\t', "^I");
-    }
-
-    // show line numbers
-    if args.number && !args.number_nonblank {
-        //     1	build:
-        //     [...]
-        //    10		cp target/release/dungeoncrawl dist
-        // There's 3/4 spaces, right, justified, so we'll need some format! love
-        // to get this right
-        println!("{: >6} {}", number, s);
-    } else if args.number_nonblank {
-        if s.is_empty() {
-            println!();
-        } else {
-            println!("{: >6} {}", number, s);
-        }
-    } else {
-        println!("{}", s);
-    }
+fn push_caret<T: Write>(stdout: &mut T, _stderr: &mut std::io::StderrLock, notation: u8) {
+    stdout.write_all(&[b'^']).unwrap();
+    stdout.write_all(&[notation]).unwrap();
 }
